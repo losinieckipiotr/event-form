459a0147887bc3a33e2bd9eebddfc0445818858a
Piotr Losiniecki <P.Losiniecki@adbglobal.com>
2020-10-05 20:57:07 +0200

    frontend v1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,9 @@ import Form from '../form/Form';
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <h1>Event Form</h1>
+      </header>
       <Form/>
      </div>
   );
--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,29 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import email from "email-validator";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+
+function notEmpty(v: string): boolean {
+  return v !== ''
+}
+
+const  isValidEmail = email.validate;
 
 export default function Form() {
+  const [ firstName, setFirstName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ date, setDate ] = useState<Date | undefined>(undefined);
+  const [ formValid, setFormValid ] = useState(false);
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const s = JSON.stringify({ firstName, lastName, email, date, formValid });
+    console.log(s);
+
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setDate(undefined);
+  }
+
+  useEffect(() => {
+    const valid = [
+      notEmpty(firstName),
+      notEmpty(lastName),
+      isValidEmail(email),
+      date !== undefined
+    ].reduce((p, c) => p && c);
+
+    setFormValid(valid);
+
+    console.log({
+      firstName,
+      lastName,
+      email,
+      date,
+      formValid: valid,
+    });
+
+  }, [firstName, lastName, email, date]);
+
   return (
-    <form onSubmit={() => { alert("submit") }}>
-      <p>
+    <form onSubmit={onSubmit}>
+      <div>
         <label>First name</label>
         <br/>
-        <input type="text"></input>
-      </p>
-      <p>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div>
         <label>Last name</label>
         <br/>
-        <input type="text"></input>
-      </p>
-      <p>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div>
         <label>Email</label>
         <br/>
-        <input type="text"></input>
-      </p>
-      <p>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
         <label>Date</label>
         <br/>
-        <input type="date"></input>
-      </p>
-      <button type="submit">Submit</button>
+        <DatePicker
+          selected={date}
+          onChange={(d: Date) => setDate(d)}
+        />
+      </div>
+      <button disabled={!formValid} type="submit" >Submit</button>
     </form>
   );
 }
